fd3e321dfb977f0a912f662f743d22da8d19f6e8
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-19 22:00:54 +0300

    Initial Commit

---
+++ SwiftData60day/SwiftData60dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData60dayApp.swift
+//  SwiftData60day
+//
+//  Created by Marat Fakhrizhanov on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData60dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
