29c2b94b83caf949f75b719d7f1cdd6ddd01a110
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-19 22:09:57 +0300

    add model

--- SwiftData60day/User.swift
+++ SwiftData60day/User.swift
@@ -5,4 +5,20 @@
 //  Created by Marat Fakhrizhanov on 19.10.2024.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+class User {
+    var id: String
+    var isActive: String
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var adress: String
+    var about: String
+    var registered: String
+    var tags: String
+    
+}
