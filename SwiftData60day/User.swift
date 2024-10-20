fb433b1061623ba532277a8f33c3e5d5eeb9fb15
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-20 15:49:38 +0300

    fixed

--- SwiftData60day/User.swift
+++ SwiftData60day/User.swift
@@ -5,35 +5,21 @@
 //  Created by Marat Fakhrizhanov on 19.10.2024.
 //
 
-import SwiftData
+
 import SwiftUI
 
-@Model
-class User: Codable {
+
+struct User: Codable, Identifiable {
     var id: String
-    var isActive: String
+    var isActive: Bool
     var name: String
     var age: Int
     var company: String
     var email: String
-    var adress: String
+    var address: String
     var about: String
     var registered: String
-    var tags: String
+    var tags: [String]
     var friends: [Friend]
-    
-    init(id: String, isActive: String, name: String, age: Int, company: String, email: String, adress: String, about: String, registered: String, tags: String, friends: [Friend]) {
-        self.id = id
-        self.isActive = isActive
-        self.name = name
-        self.age = age
-        self.company = company
-        self.email = email
-        self.adress = adress
-        self.about = about
-        self.registered = registered
-        self.tags = tags
-        self.friends = friends
-    }
-    // Friend ?
+ 
 }
